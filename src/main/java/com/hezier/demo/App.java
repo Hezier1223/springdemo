17b2925bd8d9d07b0f7b0e709dfb25f0ee20f435
朱正浩 <arthur_zzh@126.com>
2017-11-20 22:27:08 +0800

    add log4j

--- src/main/java/com/hezier/demo/App.java
+++ src/main/java/com/hezier/demo/App.java
@@ -1,12 +1,15 @@
 package com.hezier.demo;
 
 import com.hezier.demo.controller.UserController;
+import org.apache.log4j.Logger;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
+    static Logger log = Logger.getLogger(App.class.getName());
 
     public static void main(String[] args) {
+        log.info("Going to create HelloWord Obj");
         String[] ID = {"10208", "10609", "10331"};
         System.out.println(ID.length);
         // TODO Auto-generated method stub
