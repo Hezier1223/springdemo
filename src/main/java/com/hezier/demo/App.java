a1e724c0c385cefcaeb92e5582eb378984c2ed73
朱正浩 <arthur_zzh@126.com>
2017-11-20 23:21:21 +0800

    add log4j

--- src/main/java/com/hezier/demo/App.java
+++ src/main/java/com/hezier/demo/App.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
-    static Logger log = Logger.getLogger(App.class.getName());
+    private static Logger log = Logger.getLogger(App.class.getName());
 
     public static void main(String[] args) {
         log.info("Going to create HelloWord Obj");
